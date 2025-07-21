edff4ada44b43215ec4726d95d5002679ebeb92f
Timo Dittmann <timo.dittmann@mail.schwarz>
2025-07-21 10:28:41 +0200

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,43 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      filename: 'service-worker.js',
+      manifest: {
+        name: "Vue PWA Example",
+        short_name: "VPE",
+        description: "A super installable app created with Vue",
+        icons: [
+          {src: '/app-icons/icon-48x48.png', sizes: '48x48', type: 'image/png'},
+          {src: '/app-icons/icon-72x72.png', sizes: '72x72', type: 'image/png'},
+          {src: '/app-icons/icon-96x96.png', sizes: '96x96', type: 'image/png'},
+          {src: '/app-icons/icon-128x128.png', sizes: '128x128', type: 'image/png'},
+          {src: '/app-icons/icon-144x144.png', sizes: '144x144', type: 'image/png'},
+          {src: '/app-icons/icon-152x152.png', sizes: '152x152', type: 'image/png'},
+          {src: '/app-icons/icon-192x192.png', sizes: '192x192', type: 'image/png'},
+          {src: '/app-icons/icon-256x256.png', sizes: '256x256', type: 'image/png'},
+          {src: '/app-icons/icon-384x384.png', sizes: '384x384', type: 'image/png'},
+          {src: '/app-icons/icon-512x512.png', sizes: '512x512', type: 'image/png'},
+        ],
+        screenshots: [
+          {src: 'app-screenshots/screenshot-1.png', sizes: "1080x1903", type: 'image/png'}
+        ]
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
