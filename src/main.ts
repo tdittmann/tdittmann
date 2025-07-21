637f3d95ecbaffa29484ff53807ab16bd2596ea1
Timo Dittmann <timo.dittmann@mail.schwarz>
2025-07-21 10:34:03 +0200

    Fix formatting

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
-  .use(createPinia())
-  .use(router)
+const app = createApp(App).use(createPinia()).use(router)
 
 app.mount('#app')
