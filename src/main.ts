5674f71f708a2c374808cbdb35ad44fccbcb1e37
Timo Dittmann <timo.dittmann@mail.schwarz>
2025-07-23 13:03:18 +0200

    Fix code format

--- src/main.ts
+++ src/main.ts
@@ -20,13 +20,15 @@ const i18n = createI18n({
     de: {
       home: {
         title: 'Hello World 👋',
-        aboutMe: 'Ich bin Timo Dittmann, Full-Stack Software Engineer mit über zehn Jahren Berufserfahrung in der Entwicklung komplexer Anwendungen. In dieser Zeit habe ich mit einer Vielzahl von Programmiersprachen und Technologien gearbeitet – aktuell liegt mein Fokus auf Vue und Go. Neben meiner praktischen Erfahrung habe ich auch mehrere anerkannte Zertifizierungen erworben, darunter "Oracle Certified Professional – Java SE Programmer" sowie "iSAQB Certified Professional for Software Architecture – Advanced Level".',
+        aboutMe:
+          'Ich bin Timo Dittmann, Full-Stack Software Engineer mit über zehn Jahren Berufserfahrung in der Entwicklung komplexer Anwendungen. In dieser Zeit habe ich mit einer Vielzahl von Programmiersprachen und Technologien gearbeitet – aktuell liegt mein Fokus auf Vue und Go. Neben meiner praktischen Erfahrung habe ich auch mehrere anerkannte Zertifizierungen erworben, darunter "Oracle Certified Professional – Java SE Programmer" sowie "iSAQB Certified Professional for Software Architecture – Advanced Level".',
       },
     },
     en: {
       home: {
         title: 'Hello World 👋',
-        aboutMe: 'I am Timo Dittmann a full-stack software engineer with over ten years of professional experience in the development of complex applications. During this time I have worked with a variety of programming languages and technologies - currently my focus is on Vue and Go. In addition to my practical experience, I have also acquired several recognised certifications, including "Oracle Certified Professional - Java SE Programmer" and "iSAQB Certified Professional for Software Architecture - Advanced Level"',
+        aboutMe:
+          'I am Timo Dittmann a full-stack software engineer with over ten years of professional experience in the development of complex applications. During this time I have worked with a variety of programming languages and technologies - currently my focus is on Vue and Go. In addition to my practical experience, I have also acquired several recognised certifications, including "Oracle Certified Professional - Java SE Programmer" and "iSAQB Certified Professional for Software Architecture - Advanced Level"',
       },
     },
   },
